b467e70cf10041bf99aa3e03c677f731bc084f15
Wyatt Munson <munsonwf@gmail.com>
2024-01-21 15:35:33 -0800

    fix: export version number

--- images/node/scripts/semrel-node.sh
+++ images/node/scripts/semrel-node.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# source ./ckvar.sh
+BASE_PATH=${BASE_PATH:-"/opt/wm/scripts"}
+source ${BASE_PATH}/checks/ckvar.sh
+source ${BASE_PATH}/checks/git-check.sh
 
+pwd
+ls
+
+echo "sourcing from ${BASE_PATH}/checks/ckvar.sh"
 
 # CHECK / FIND CODEBASE
 # TODO: send warning if not provided
 REPO_LOCATION=${REPO_LOCATION:-"."}
 cd $REPO_LOCATION
 
+# CHECK FOR VARS
+
+ckvar "GITHUB_TOKEN" "soft"
+
 # CHECK FOR GIT REPO
+git_check
 
 # RUN SEMANTIC RELEASE
-npx semantic-release
+npx semantic-release | tee semver-output.txt
+
+# get version from release
+export NEXT_VERSION=$(cat semver-output.txt | grep "The next release version is" | grep -Eo '\d+\.\d+\.\d+')
+# PREVIOUS_VERSION=$(cat version.txt | grep "associated with version" | grep -Eo '\d+\.\d+\.\d+')
+
