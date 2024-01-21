d93ae63e8b14e2894fe16c886c57ed02df2b3c87
Wyatt Munson <munsonwf@gmail.com>
2024-01-20 18:52:00 -0800

    feat: add gh clone step

---
+++ images/alpine/scripts/gh-clone.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+BASE_PATH=${BASE_PATH:-"./scripts"}
+source ${BASE_PATH}/prints.sh 
+log "start" "Clone git repo"
+log "info" "Setting git config..."
+
+TEST_CMD="source ${BASE_PATH}/check-var.sh"
+alias ckvar="$TEST_CMD"
+ckvar GIT_REPO_NAME "debug"
+ckvar GIT_PAT_TOKEN "debug"
+
+# Authenticate
+log "info" "Authenticating..."
+echo $GIT_PAT_TOKEN > .GIT_PAT_TOKEN
+gh auth login --with-token < .GIT_PAT_TOKEN
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" > .GIT_PAT_TOKEN
+rm .GIT_PAT_TOKEN
+
+# wyattmunson/private-repo 
+# https://github.com/wyattmunson/private-repo.git
+
+gh repo clone ${GIT_REPO_NAME}
---
+++ images/alpine/scripts/git-clone.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# BASE_PATH=images/alpine/scripts
+BASE_PATH=${BASE_PATH:-"./scripts"}
+source ${BASE_PATH}/prints.sh 
+log "start" "Git config"
+log "info" "Setting git config..."
+
+# git config --global url."https://your_username:$GIT_AUTH_TOKEN@github.com/".insteadOf "https://github.com/"
+echo "source ${BASE_PATH}/check-var.sh"
+TEST_CMD="source ${BASE_PATH}/check-var.sh"
+alias ckvar="$TEST_CMD"
+ckvar GIT_PAT_USERNAME "debug"
+ckvar GIT_PAT_TOKEN "debug"
+ckvar GIT_ORG_NAME "debug"
+ckvar GIT_REPO_NAME "debug"
+
+GIT_BASE_URL=${GIT_BASE_URL:-"github.com"}
+
+log "info" "Cloning repo..."
+git clone https://$GIT_AUTH_TOKEN@$GIT_BASE_URL/$GIT_ORG_NAME/$GIT_REPO_NAME.git
+# git remote set-url origin https://$GIT_PAT_USERNAME:$GIT_AUTH_TOKEN@github.com/$GIT_ORG_NAME/$GIT_REPO_NAME.git
+
+# if [[ "$0" = "0" ]]; then
+#     echo "success"
+# else
+#     echo "success"
+# fi
