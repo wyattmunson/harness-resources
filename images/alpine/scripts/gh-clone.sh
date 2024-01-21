84c6b1d3d6de7ba3c7d00cb1e6ba432b51cfb827
Wyatt Munson <munsonwf@gmail.com>
2024-01-20 19:12:14 -0800

    fix: alias

--- images/alpine/scripts/gh-clone.sh
+++ images/alpine/scripts/gh-clone.sh
@@ -7,7 +7,7 @@ log "start" "Clone git repo"
 log "info" "Setting git config..."
 
 TEST_CMD="source ${BASE_PATH}/check-var.sh"
-alias ckvar="$TEST_CMD"
+alias ckvar='$TEST_CMD'
 ckvar GIT_REPO_NAME "debug"
 ckvar GIT_PAT_TOKEN "debug"
 
